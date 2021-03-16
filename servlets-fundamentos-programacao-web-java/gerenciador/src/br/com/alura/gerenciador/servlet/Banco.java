9c56f2aa9c0b84e76c7b6af9d786941306692af9
lesfernandes <lemayara16@gmail.com>
2021-03-16 16:07:05 -0300

    completando o crud

--- servlets-fundamentos-programacao-web-java/gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
+++ servlets-fundamentos-programacao-web-java/gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,17 +1,21 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 	
 	private static List<Empresa> empresas = new ArrayList<Empresa>();
+	private static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(Banco.chaveSequencial++);
 		empresa.setNome("Google");
 		
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(Banco.chaveSequencial++);
 		empresa2.setNome("Alura");
 		
 		empresas.add(empresa);
@@ -19,12 +23,34 @@ public class Banco {
 	}
 
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		Banco.empresas.add(empresa);
 	}
 	
 	public List<Empresa> getEmpresas()
 	{
 		return Banco.empresas;
+	}
+
+	public void removeEmpresa(Integer id) {
+		Iterator<Empresa> it = empresas.iterator();
+		
+		while(it.hasNext()) {
+			Empresa empresa = it.next();
+			if (empresa.getId() == id) {
+				it.remove();
+			}
+		}
+	}
+
+	public Empresa buscaEmpresaPeloId(Integer id) {
+		for (Empresa empresa : empresas) {
+			if (empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		
+		return null;
 	}
 
 }
