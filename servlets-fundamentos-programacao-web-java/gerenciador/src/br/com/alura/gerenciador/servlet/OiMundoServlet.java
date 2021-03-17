69e3cafe57524c53c36dfb0487e4976d6350fba1
lesfernandes <lemayara16@gmail.com>
2021-03-17 14:50:26 -0300

    deploy da aplicação

--- servlets-fundamentos-programacao-web-java/gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ servlets-fundamentos-programacao-web-java/gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -8,12 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/oi")
+//@WebServlet(urlPatterns="/oi")
 public class OiMundoServlet extends HttpServlet {
+	
+	public OiMundoServlet() {
+		System.out.println("Criando oi mundo servlet");
+	}
 
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
+	  
       PrintWriter out = resp.getWriter();
       out.println("<html>");
       out.println("<body>");
